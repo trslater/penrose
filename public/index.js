f9f2d6c0561c09df14ae996a9abb98efce61cc6a
Tristan Slater <hello@trslater.ca>
2022-06-07 15:47:36 -0700

    Refactor

--- public/index.js
+++ public/index.js
@@ -1,31 +1,22 @@
-const initSize = 600
-const phi = 0.618
+const INIT_SIZE = 600
+const PHI = 0.618
+const THETA = 3.14159/5
 
-let wedges = []
+let piePieces
+let aabb
 
 function setup() {
-    for (let i = 0; i < 10; ++i) {
-        const u = new p5.Vector(initSize*cos((i - 1)*2*PI/10), initSize*sin((i - 1)*2*PI/10))
-        const v = new p5.Vector(initSize*cos(i*2*PI/10), initSize*sin(i*2*PI/10))
-        
-        const a = new p5.Vector(0, 0)
-        const b = p5.Vector.add(a, u)
-        const c = p5.Vector.add(a, v)
-        
-        const points = (i%2 == 0) ? [a, b, c] : [a, c, b]
+    piePieces = [...Array(10).keys()].map(piePiece)
 
-        wedges.push(subdivide(...points, true, 6))
-    }
-    
     createCanvas(700, 700, WEBGL)
     frameRate(60)
-    background(255)
 }
 
 function draw() {
-    for (const wedge of wedges) {
-        try {
-            const [a, b, c, isNarrow] = wedge.next().value
+    background(255)
+    for (const piece of piePieces) {
+        for (const fragment of piece) {
+            const [a, b, c, isNarrow] = fragment
         
             orangeAndGreen(isNarrow)
             beginShape()
@@ -33,24 +24,35 @@ function draw() {
             vertex(a.x, a.y)
             vertex(c.x, c.y)
             endShape()
-        } catch (TypeError) {
-            console.log("end")
         }
     }
+}
+
+function piePiece(i) {
+    const alpha = (i - 1)*THETA
+    const beta = i*THETA
+
+    const a = new p5.Vector(0, 0)
+    const b = new p5.Vector(INIT_SIZE*cos(alpha), INIT_SIZE*sin(alpha))
+    const c = new p5.Vector(INIT_SIZE*cos(beta), INIT_SIZE*sin(beta))
+    
+    const points = (i%2 == 0) ? [a, b, c] : [a, c, b]
+
+    return Array.from(subdivide(...points, true, 6))
 }
 
 function* subdivide(a, b, c, isNarrow, depth) {
     // Base case
     if (depth == 0) yield [a, b, c, isNarrow]
     else if (isNarrow) {
-            const p = p5.Vector.add(p5.Vector.mult(a, (1 - phi)), p5.Vector.mult(b, phi))
-    
-            yield* subdivide(p, c, a, false, depth - 1)
-            yield* subdivide(c, p, b, true, depth - 1)
+        const p = p5.Vector.add(p5.Vector.mult(a, (1 - PHI)), p5.Vector.mult(b, PHI))
+
+        yield* subdivide(p, c, a, false, depth - 1)
+        yield* subdivide(c, p, b, true, depth - 1)
     }
     else {
-        const q = p5.Vector.add(p5.Vector.mult(b, (1 - phi)), p5.Vector.mult(a, phi))
-        const r = p5.Vector.add(p5.Vector.mult(b, (1 - phi)), p5.Vector.mult(c, phi))
+        const q = p5.Vector.add(p5.Vector.mult(b, (1 - PHI)), p5.Vector.mult(a, PHI))
+        const r = p5.Vector.add(p5.Vector.mult(b, (1 - PHI)), p5.Vector.mult(c, PHI))
         
         yield* subdivide(q, r, b, false, depth - 1)
         yield* subdivide(r, q, a, true, depth - 1)
