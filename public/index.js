0e58779c9222779bd2fec614d9b1f145c0b2dc1b
Tristan Slater <hello@trslater.ca>
2022-06-08 00:57:08 -0700

    Draw curved lines

--- public/index.js
+++ public/index.js
@@ -8,23 +8,48 @@ let aabb
 function setup() {
     piePieces = [...Array(10).keys()].map(piePiece)
 
+    // for (const piece of piePieces) {
+    //     for (const fragment of piece) {
+    //         console.log(fragment)
+    //     }
+    // }
+    
     createCanvas(700, 700, WEBGL)
     frameRate(60)
+    background(255)
 }
 
 function draw() {
-    background(255)
+    // console.log(mouseX, mouseY)
+
     for (const piece of piePieces) {
-        for (const fragment of piece) {
-            const [a, b, c, isNarrow] = fragment
+        // for (const subWedge of wedge) {
+        //     const [a, b, c, isNarrow] = subWedge
         
-            orangeAndGreen(isNarrow)
-            beginShape()
-            vertex(b.x, b.y)
-            vertex(a.x, a.y)
-            vertex(c.x, c.y)
-            endShape()
-        }
+        //     orangeAndGreen(isNarrow)
+        //     beginShape()
+        //     vertex(b.x, b.y)
+        //     vertex(a.x, a.y)
+        //     vertex(c.x, c.y)
+        //     endShape()
+        // }
+        const [a, b, c, isNarrow] = piece.next().value
+    
+        // grid()
+        // orangeAndGreen(isNarrow)
+        // onlyNarrows(isNarrow)
+        // onlyWides(isNarrow)
+        // crumpledPaper()
+        // beginShape()
+        // vertex(b.x, b.y)
+        // vertex(a.x, a.y)
+        // vertex(c.x, c.y)
+        // endShape()
+
+        noFill()
+        stroke(0, 0, 0)
+        // circles1(a, b, c)
+        circles2(a, b, c, isNarrow)
     }
 }
 
@@ -38,7 +63,7 @@ function piePiece(i) {
     
     const points = (i%2 == 0) ? [a, b, c] : [a, c, b]
 
-    return Array.from(subdivide(...points, true, 6))
+    return subdivide(...points, true, 7)
 }
 
 function* subdivide(a, b, c, isNarrow, depth) {
@@ -60,6 +85,11 @@ function* subdivide(a, b, c, isNarrow, depth) {
     }
 }
 
+function grid() {
+    strokeWeight(1)
+    noFill()
+}
+
 function orangeAndGreen(isNarrow) {
     strokeWeight(1)
     if (isNarrow) fill(255, 200, 150)
@@ -67,9 +97,63 @@ function orangeAndGreen(isNarrow) {
     stroke(100, 100, 50)
 }
 
+function onlyNarrows(isNarrow) {
+    if (isNarrow) {
+        strokeWeight(1)
+        fill(150, 255, 200)
+        stroke(100, 100, 50)
+    } else {
+        noStroke()
+        noFill()
+    }
+}
+
+function onlyWides(isNarrow) {
+    if (!isNarrow) {
+        strokeWeight(1)
+        fill(150, 255, 200)
+        stroke(100, 100, 50)
+    } else {
+        noFill()
+        noStroke()
+    }
+}
+
 function crumpledPaper() {
     const p = Math.random()
 
     noStroke()
     fill((1 - p)*100 + p*200)
+}
+
+function circles1(a, b, c) {
+    const r = sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2))
+
+    arcBetween(a, b, c, r)
+}
+
+function circles2(a, b, c, isNarrow) {
+    const r = sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2))
+    
+    if (isNarrow) {
+        arcBetween(a, b, c, r)
+    }
+    else {
+        arcBetween(b, a, c, r)
+        arcBetween(c, a, b, r)
+    }
+}
+
+function arcBetween(a, b, c, r) {
+    let alpha = atan2(c.y - a.y, c.x - a.x)
+    let beta = atan2(b.y - a.y, b.x - a.x)
+
+    if (alpha < 0) alpha += 2*PI
+    if (beta < 0) beta += 2*PI
+    
+    if (max(alpha, beta) - min(alpha, beta) > PI) {
+        arc(a.x, a.y, r, r, max(alpha, beta), min(alpha, beta))
+    } else {
+        arc(a.x, a.y, r, r, min(alpha, beta), max(alpha, beta))
+    }
 }
